a84252116e8fe153550b10a9e9bac92ea9c0edfe
EugenM <eugen.m@gowombat.team>
2021-03-02 15:07:39 +0200

    update index.html

--- js/main.js
+++ js/main.js
@@ -4,15 +4,6 @@ var DemoCMP = {},
 
 DemoCMP.LoadCMP = function () {
     tileNavigation = new Navigation();
-    try {
-        logger.debug('Widget version: ' + window.tizen.application.getAppInfo().version);
-    } catch (e) {
-        try {
-            logger.debug('Widget version: ' + window.tizen.application.getAppInfo());
-        } catch (e) {
-            logger.debug('Tizen API is not reachable')
-        }
-    }
 };
 
 window.onload = DemoCMP.LoadCMP();
