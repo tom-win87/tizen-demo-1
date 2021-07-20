0d2beb7f1b849622cac3c423f894734bb4ebb263
EugenM <eugen.m@gowombat.team>
2021-07-20 13:34:28 +0300

    fix Enter key event handler
    (cherry picked from commit 8091bfefb424420bb1aa1002dc788efa0d30780f)

--- js/navigation.js
+++ js/navigation.js
@@ -3,64 +3,20 @@ function Navigation (options) {
     this.viewportModal = options.viewport || "[id^='sp_message_iframe_']";
     this.tvKey = {
         KEY_ENTER: 13,          // Enter
-        KEY_PAUSE: 19,          // MediaPause
         KEY_LEFT: 37,           // ArrowLeft
         KEY_UP: 38,             // ArrowUp
         KEY_RIGHT: 39,          // ArrowRight
         KEY_DOWN: 40,           // ArrowDown
         KEY_0: 48,              // 0
         KEY_1: 49,              // 1
-        KEY_2: 50,              // 2
-        KEY_3: 51,              // 3
-        KEY_4: 52,              // 4
-        KEY_5: 53,              // 5
-        KEY_6: 54,              // 6
-        KEY_7: 55,              // 7
-        KEY_8: 56,              // 8
-        KEY_9: 57,              // 9
-        KEY_EMPTY: 189,         // Minus
-        KEY_RED: 403,           // ColorF0Red
-        KEY_GREEN: 404,         // ColorF1Green
-        KEY_YELLOW: 405,        // ColorF2Yellow
-        KEY_BLUE: 406,          // ColorF3Blue
-        KEY_RW: 412,            // MediaRewind
-        KEY_STOP: 413,          // MediaStop
-        KEY_PLAY: 415,          // MediaPlay
-        KEY_REC: 416,           // MediaRecord
-        KEY_FF: 417,            // MediaFastForward
-        KEY_CH_UP: 427,         // ChannelUp
-        KEY_CH_DOWN: 428,       // ChannelDown
-        KEY_VOL_UP: 447,        // VolumeUp
-        KEY_VOL_DOWN: 448,      // VolumeDown
-        KEY_MUTE: 449,          // VolumeMute
-        KEY_INFO: 457,          // Info
-        KEY_GUIDE: 458,         // Guide
-        KEY_RETURN: 10009,      //
-        KEY_SOURCE: 10072,      // Source
-        KEY_CHLIST: 10073,      // ChannelList
-        KEY_MENU: 10133,        // Menu
-        KEY_TOOLS: 10135,       // Tools
-        KEY_ASPECT: 10140,      // PictureSize
-        KEY_EMANUAL: 10146,     // E-Manual
+        KEY_RETURN: 10009,      // Back
         KEY_EXIT: 10182,        // Exit
-        KEY_PRECH: 10190,       // PreviousChannel
-        KEY_MTS: 10195,         // MTS
-        KEY_3D: 10199,          // 3D
-        KEY_TTX_MIX: 10200,     // Teletext
-        KEY_CAPTION: 10221,     // Caption
-        KEY_SEARCH: 10225,      // Search
-        KEY_SOCCER: 10228,      // Soccer
-        KEY_REWIND_: 10232,     // MediaTrackPrevious
-        KEY_FF_: 10233,         // MediaTrackNext
-        KEY_PLAY_PAUSE: 10252,  // MediaPlayPause
-        KEY_EXTRA: 10253        // Extra
     };
     this.onLoad();
 }
 
 Navigation.prototype = {
     onLoad: function () {
-        //this.registerAllKey();
         this.registerKey('0');
         this.registerKey('1');
         this.bindEvents();
@@ -95,8 +51,11 @@ Navigation.prototype = {
         const activeElement = this.getActiveElement();
         let enterEvent = new Event('keydown', {bubbles: true, cancelable: true});
         enterEvent.keyCode = this.tvKey.KEY_ENTER;
-        if (activeElement.style.cssText === 'font-weight: normal;'){
+        if (activeElement.parentElement.className.split(' ').indexOf('categories') >= 0 ){
             enterEvent = new KeyboardEvent('keypress',{key: 'Enter', bubbles: true, charCode: 0, keyCode: this.tvKey.KEY_ENTER});
+        }
+        if (activeElement.tagName === 'BUTTON' && activeElement.className.split(' ').indexOf('message-button') >=0){
+            enterEvent = new KeyboardEvent('keydown',{key: 'Enter', bubbles: true, charCode: 0, keyCode: this.tvKey.KEY_ENTER, code: "Enter"});
         }
         activeElement.dispatchEvent(enterEvent);
     },
