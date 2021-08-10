2d060b6385af1d9f16d024b219e6c7151881d1ed
EugenM <eugen.m@gowombat.team>
2021-08-10 14:19:21 +0300

    add some device info printing to the console

--- js/main.js
+++ js/main.js
@@ -4,7 +4,10 @@ var DemoCMP = {},
 DemoCMP.LoadCMP = function () {
     tileNavigation = new Navigation();
     try {
-        console.log('Widget version: '+ window.tizen.application.getAppInfo().version);
+        console.log('Widget version:', window.tizen.application.getAppInfo().version);
+        console.log('Manufacturer:', window.tizen.systeminfo.getCapability('http://tizen.org/system/manufacturer'));
+        console.log('Model name:', window.tizen.systeminfo.getCapability('http://tizen.org/system/model_name'));
+        console.log('Platform:', window.tizen.systeminfo.getCapability('http://tizen.org/system/platform.name'), window.tizen.systeminfo.getCapability('http://tizen.org/feature/platform.version'));
     } catch (e) {
         console.log(e);
     }
